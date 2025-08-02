9cf248200ba1ac26bb09d4970958443ba5bcfb8a
Ratinan Lee <lee.ratinan@gmail.com>
2025-08-02 23:49:21 +0800

    blog view

--- app/Views/blog-view.php
+++ app/Views/blog-view.php
@@ -2,15 +2,21 @@
 $this->extend('_layout.php');
 $this->section('content');
 ?>
-    <section id="about" class="about section" style="margin-top:100px !important;">
-        <div class="container">
+    <div class="page-title">
+        <div class="heading">
+            <div class="container">
+                <div class="row d-flex justify-content-center text-center">
+                    <div class="col-lg-8">
+                        <h1 class="heading-title"><?= $post['title'] ?></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section id="blog" class="blog section">
+        <div class="container aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
             <div class="row">
                 <div class="col col-md-10 col-lg-8">
-                    <!-- Section Title -->
-                    <div class="container section-title aos-init aos-animate" data-aos="fade-up">
-                        <h1><?= lang('Theme.website-name') ?></h1>
-                        <h2><?= $post['title'] ?></h2>
-                    </div><!-- End Section Title -->
                     <p><a href="<?= base_url($locale . '/blog') ?>"><?= lang('Theme.pages.blog') ?></a> / <?= $post['title'] ?> /</p>
                     <?php if (!empty($post['media'])) : ?>
                         <img src="<?= $post['media']['media_details']['sizes']['full']['source_url'] ?>" alt="<?= $post['title'] ?>" class="img-fluid" />
