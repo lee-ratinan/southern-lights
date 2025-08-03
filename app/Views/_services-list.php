b49d2f1798d1ca8c1c7f2e85528ad69f4517a8b0
Ratinan Lee <lee.ratinan@gmail.com>
2025-08-03 21:06:05 +0800

    add links to service

--- app/Views/_services-list.php
+++ app/Views/_services-list.php
@@ -5,14 +5,14 @@
                 <div class="specialty-meta">
                     <span class="specialty-label"><?= lang('Home.services.our-service') ?></span>
                 </div>
-                <h3><?= $service['title'] ?></h3>
+                <h3><a href="<?= base_url($locale . '/services/view?q=' . $service['slug']) ?>"><?= $service['title'] ?></a></h3>
                 <p><?= $service['excerpt'] ?></p>
                 <a href="<?= base_url($locale . '/services/view?q=' . $service['slug']) ?>" class="specialty-link">
                     <?= lang('Home.services.read-more') ?> <i class="bi bi-arrow-right"></i>
                 </a>
             </div>
             <div class="specialty-visual">
-                <img src="<?= $services['media'][$service['featured_image']] ?>" alt="Cardiovascular Medicine" class="img-fluid">
+                <a href="<?= base_url($locale . '/services/view?q=' . $service['slug']) ?>"><img src="<?= $services['media'][$service['featured_image']] ?>" alt="Cardiovascular Medicine" class="img-fluid"></a>
             </div>
         </div>
     </div>
