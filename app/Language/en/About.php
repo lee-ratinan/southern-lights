c9ade2ce288a4a60b93114d3b92f23cfb639c082
Ratinan Lee <lee.ratinan@gmail.com>
2025-08-03 12:05:50 +0800

    about us translation

---
+++ app/Language/en/About.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'header'   => 'Compassionate Care for All',
+    'para1'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
+    'para2'    => 'Nam odio sem, volutpat non pharetra eu, ornare eu arcu. Pellentesque sollicitudin, ex id rutrum mollis, felis felis pellentesque metus, et cursus felis sem id mauris. Vivamus vel nunc ullamcorper, iaculis augue vitae, malesuada arcu.',
+    'counter1' => 'Established',
+    'counter2' => 'Friendly Therapists',
+    'counter3' => '???',
+    'alt1'     => 'Sky Thai Spa interior',
+    'alt2'     => 'Sky Thai Spa friendly staff',
+];
--- app/Views/about-us.php
+++ app/Views/about-us.php
@@ -18,30 +18,30 @@ $this->section('content');
             <div class="row align-items-center">
                 <div class="col-lg-6 aos-init aos-animate" data-aos="fade-right" data-aos-delay="100">
                     <div class="about-content">
-                        <h2>Compassionate Care for All</h2>
-                        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        <div class="stats-grid d-none">
+                        <h2><?= lang('About.header') ?></h2>
+                        <p class="lead"><?= lang('About.para1') ?></p>
+                        <p><?= lang('About.para2') ?></p>
+                        <div class="stats-grid">
                             <div class="stat-item">
-                                <span class="stat-number" data-purecounter-start="0" data-purecounter-end="15000" data-purecounter-duration="2">999</span>
-                                <span class="stat-label">xxx</span>
+                                <span class="stat-number" data-purecounter-start="0" data-purecounter-end="2025" data-purecounter-duration="2">2025</span>
+                                <span class="stat-label"><?= lang('About.counter1') ?></span>
                             </div>
                             <div class="stat-item">
-                                <span class="stat-number" data-purecounter-start="0" data-purecounter-end="25" data-purecounter-duration="2">999</span>
-                                <span class="stat-label">xxx</span>
+                                <span class="stat-number" data-purecounter-start="0" data-purecounter-end="10" data-purecounter-duration="2">10</span>
+                                <span class="stat-label"><?= lang('About.counter2') ?></span>
                             </div>
-                            <div class="stat-item">
+                            <div class="stat-item d-none">
                                 <span class="stat-number" data-purecounter-start="0" data-purecounter-end="50" data-purecounter-duration="2">999</span>
-                                <span class="stat-label">xxx</span>
+                                <span class="stat-label"><?= lang('About.counter3') ?></span>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6 aos-init aos-animate" data-aos="fade-left" data-aos-delay="200">
                     <div class="image-wrapper">
-                        <img src="<?= base_url('assets/img/about/about.jpg') ?>" class="img-fluid main-image" alt="xxxxxxxx">
+                        <img src="<?= base_url('assets/img/about/about.jpg') ?>" class="img-fluid main-image" alt="<?= lang('About.alt1') ?>">
                         <div class="floating-image aos-init aos-animate" data-aos="zoom-in" data-aos-delay="400">
-                            <img src="<?= base_url('assets/img/about/person.webp') ?>" class="img-fluid" alt="xxxxxxxxx">
+                            <img src="<?= base_url('assets/img/about/person.webp') ?>" class="img-fluid" alt="<?= lang('About.alt2') ?>">
                         </div>
                     </div>
                 </div>
