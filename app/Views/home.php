4c250910384f745d4799ee67fda00fadb570193b
Ratinan Lee <lee.ratinan@gmail.com>
2025-08-06 20:32:38 +0800

    css fix

--- app/Views/home.php
+++ app/Views/home.php
@@ -94,13 +94,13 @@ $this->section('content');
     <section id="featured-departments" class="featured-departments section">
         <div class="container section-title aos-init aos-animate" data-aos="fade-up">
             <h2><?= lang('Home.services.heading') ?></h2>
-            <p><?= lang('Home.services.para') ?></p>
+            <p class="mb-3"><?= lang('Home.services.para') ?></p>
+            <a class="btn btn-bg btn-outline-primary rounded-pill px-5" href="<?= base_url($locale . '/services/') ?>">+ <?= lang('Home.services.more-services') ?> +</a>
         </div>
         <div class="container aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
             <div class="row g-5">
                 <?php include "_services-list.php"; ?>
                 <div class="col-12 text-center">
-                    <a class="btn btn-bg btn-outline-primary rounded-pill px-5" href="<?= base_url($locale . '/services/') ?>">+ <?= lang('Home.services.more-services') ?> +</a>
                     <div class="pt-3">
                         <?php include "_book_and_buy.php"; ?>
                     </div>
