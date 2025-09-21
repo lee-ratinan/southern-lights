305fb935efda55343e0b97d1e0ccb2406a6eeaf4
Ratinan Lee <lee.ratinan@gmail.com>
2025-09-21 13:41:16 +0800

    Contact translation

--- app/Language/es/Contact.php
+++ app/Language/es/Contact.php
@@ -1,11 +1,21 @@
 <?php
 return [
+    'info' => [
+        'address'      => 'Nuestra dirección',
+        'email'        => 'Correo electrónico',
+        'phone'        => 'Número de teléfono',
+        'hours'        => 'Horario de atención',
+        'closed'       => 'Cerrado',
+        'differ-on-ph' => 'El horario puede variar en días festivos'
+    ],
     'form' => [
         'title'     => 'Formulario de contacto',
-        'name'      => 'Nombre',
+        'subtitle'  => '¿Tienes preguntas o quieres saber más? Contáctanos y nuestro equipo te responderá en breve.',
+        'name'      => 'Nombre *',
         'email'     => 'Correo electrónico',
-        'phone'     => 'Teléfono',
-        'message'   => 'Mensaje',
+        'phone'     => 'Teléfono *',
+        'subject'   => 'Asunto del mensaje',
+        'message'   => 'Mensaje *',
         'send'      => 'Enviar',
         'responses' => [
             'error'   => 'Lo sentimos, algo salió mal. Inténtalo de nuevo más tarde.',
--- app/Language/zh/Contact.php
+++ app/Language/zh/Contact.php
@@ -1,11 +1,21 @@
 <?php
 return [
+    'info' => [
+        'address'      => '我们的地址',
+        'email'        => '电子邮件地址',
+        'phone'        => '电话号码',
+        'hours'        => '营业时间',
+        'closed'       => '休息',
+        'differ-on-ph' => '公共假日营业时间可能会有所不同。'
+    ],
     'form' => [
         'title'     => '联系表单',
-        'name'      => '姓名',
+        'subtitle'  => '有疑问或想了解更多信息？请联系我们，我们的团队将尽快回复您。',
+        'name'      => '姓名 *',
         'email'     => '邮箱',
-        'phone'     => '电话',
-        'message'   => '留言',
+        'phone'     => '电话 *',
+        'subject'   => '消息主题',
+        'message'   => '留言 *',
         'send'      => '发送',
         'responses' => [
             'error'   => '抱歉，出现错误。请稍后重试。',
