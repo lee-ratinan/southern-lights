5d899fae3f2abcfc30e9fa06ab0ef79ce28207cd
Ratinan Lee <lee.ratinan@gmail.com>
2025-09-21 13:09:23 +0800

    Home translation

---
+++ app/Language/es/Home.php
@@ -0,0 +1,45 @@
+<?php
+return [
+    'hero'              => [
+        'badge1'   => 'Tailandés auténtico',
+        'badge2'   => 'Servicio de excelencia',
+        'message1' => 'Armonía <span class="highlight">tailandesa</span><br> de altura',
+        'message2' => 'Experimente la auténtica curación tailandesa en el corazón de Auckland, donde cada toque reconforta y cada visita restaura el equilibrio'
+    ],
+    'opening-hours-app' => [
+        'open-now'  => 'Estamos abiertos ahora',
+        'open-msg'  => '¡Reserva tu sesión ahora!',
+        'close-now' => 'Actualmente cerrado',
+        'close-msg' => 'Horario:'
+    ],
+    'services'          => [
+        'heading'       => 'Servicios Destacados',
+        'para'          => 'Auténtico Masaje Tailandés: Relájate, Recupérate y Reequilibra Hoy',
+        'our-service'   => 'Nuestro Servicio',
+        'read-more'     => 'Leer Más',
+        'more-services' => 'Más Servicios',
+    ],
+    'staff'             => [
+        'heading'   => 'Nuestro personal',
+        'new'       => 'NUEVO',
+        'book-with' => 'Reserva ahora con {0}'
+    ],
+    'permanent-promo'   => [
+        'heading' => 'Nuestras promociones',
+        'para'    => '¡Disfruta de nuestras ofertas especiales hoy!'
+    ],
+    'facilities'        => [
+        'heading'  => 'Instalaciones',
+        'facility' => 'Instalaciones de spa',
+        'shower'   => [
+            'heading' => 'Ducha gratuita',
+            'para'    => 'Disfruta de una ducha gratuita con una reserva de 1 hora o más, refréscate y relájate con nosotros'
+        ],
+        'parking'  => [
+            'heading' => 'Plaza de aparcamiento',
+            'para'    => 'Relájate: el aparcamiento está cerca y es muy fácil',
+            'map'     => 'Cómo llegar'
+        ],
+    ],
+    'popup-btn'         => 'No volver a mostrar'
+];
---
+++ app/Language/zh/Home.php
@@ -0,0 +1,45 @@
+<?php
+return [
+    'hero'              => [
+        'badge1'   => '正宗泰式',
+        'badge2'   => '优质服务',
+        'message1' => '天际<br><span class="highlight">泰式</span>和谐',
+        'message2' => '在奥克兰市中心体验正宗泰式疗愈——每一次触碰都令人舒缓，每一次疗程都恢复平衡。'
+    ],
+    'opening-hours-app' => [
+        'open-now'  => '我们现在营业。',
+        'open-msg'  => '立即预订您的课程！',
+        'close-now' => '目前已关闭。',
+        'close-msg' => '营业时间：'
+    ],
+    'services'          => [
+        'heading'       => '特色服务',
+        'para'          => '正宗泰式按摩——今天放松身心，恢复活力，重新平衡。',
+        'our-service'   => '我们的服务',
+        'read-more'     => '了解更多',
+        'more-services' => '更多服务',
+    ],
+    'staff'             => [
+        'heading'   => '我们的员工',
+        'new'       => '新品',
+        'book-with' => '立即通过 {0} 预订'
+    ],
+    'permanent-promo'   => [
+        'heading' => '我们的促销活动',
+        'para'    => '立即享受我们的特别优惠！'
+    ],
+    'facilities'        => [
+        'heading'  => '设施',
+        'facility' => '水疗设施',
+        'shower'   => [
+            'heading' => '免费淋浴',
+            'para'    => '预订1小时或以上即可享受免费淋浴，与我们一起放松身心，重焕活力。'
+        ],
+        'parking'  => [
+            'heading' => '停车位',
+            'para'    => '放松身心——停车场就在附近，交通便利。',
+            'map'     => '获取路线'
+        ],
+    ],
+    'popup-btn'         => '不再显示'
+];
