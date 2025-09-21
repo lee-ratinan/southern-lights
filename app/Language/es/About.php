21ec9b2e6ebe6cd70149766d1926bb262161cb95
Ratinan Lee <lee.ratinan@gmail.com>
2025-09-21 13:14:31 +0800

    About translation

---
+++ app/Language/es/About.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'header' => 'Atención compasiva para todos',
+    'para1'  => 'Ubicado en Remuera Road, en el corazón de Auckland, nuestro spa de masaje tailandés de nueva construcción es un santuario de paz diseñado para ayudarle a relajarse y rejuvenecer. Desde el momento en que entre, encontrará un espacio seguro y tranquilo donde la comodidad moderna se fusiona con el arte atemporal de la curación tradicional tailandesa',
+    'para2'  => 'Nuestro equipo de masajistas tailandeses cualificados aporta años de auténtica experiencia y cuidado a cada tratamiento. Con manos delicadas, profundo conocimiento y pasión por el bienestar, estamos aquí para aliviar el estrés, restablecer el equilibrio y dejarle con una sensación de renovación. Ya sea que busque relajación o alivio terapéutico, encontrará la auténtica hospitalidad tailandesa aquí en Auckland.',
+    'counter1' => 'Establecido',
+    'counter2' => 'Terapeutas amables',
+    'counter3' => '???',
+    'alt1'     => 'Interior del Sky Thai Spa',
+    'alt2'     => 'Personal amable del Sky Thai Spa',
+];
